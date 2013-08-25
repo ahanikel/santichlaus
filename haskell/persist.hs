7b4bff9016c3708d408591b388ed9386b62e3dc5
Axel Hanikel <ahanikel@gmail.com>
2013-08-25 23:05:03 +0200

    Added getRegistrationsAsJson

--- haskell/persist.hs
+++ haskell/persist.hs
@@ -8,35 +8,51 @@ import qualified Data.Text as T (unpack)
 
 saveRegistration :: Registration -> IO ()
 saveRegistration r = do
-	hReg <- openFile "registrations.txt" AppendMode
-	hTimes <- openFile "times.txt" AppendMode
-	hPutStrLn hReg $ show r
-	hPutStrLn hTimes $ T.unpack $ zeit r
-	hClose hTimes
-	hClose hReg
+    hReg <- openFile "registrations.txt" AppendMode
+    hTimes <- openFile "times.txt" AppendMode
+    hPutStrLn hReg $ show r
+    hPutStrLn hTimes $ T.unpack $ zeit r
+    hClose hTimes
+    hClose hReg
 
 bookedTimes :: IO TimeCount
 bookedTimes = do
-	hTimes <- openFile "times.txt" ReadMode
-	contents <- hGetContents hTimes
-	return $ foldl' insert Map.empty $ lines contents
-	where insert m line =
-		let count = Map.findWithDefault 0 line m
-		in Map.insert line (count + 1) m
+    hTimes <- openFile "times.txt" ReadMode
+    contents <- hGetContents hTimes
+    return $ foldl' insert Map.empty $ lines contents
+    where insert m line =
+            let count = Map.findWithDefault 0 line m
+            in Map.insert line (count + 1) m
 
 isTimeAvailable :: String -> IO Bool
 isTimeAvailable t = do
-	let max = Map.findWithDefault 0 t maxTimes
-	booked <- bookedTimes
-	let current = Map.findWithDefault 0 t booked
-	return $ (max - current) > 0
+    let max = Map.findWithDefault 0 t maxTimes
+    booked <- bookedTimes
+    let current = Map.findWithDefault 0 t booked
+    return $ (max - current) > 0
 
 availableTimes :: IO [String]
 availableTimes = do
-	booked <- bookedTimes
-	let diffFn a b =
-		if diff <= 0 then Nothing
-		else Just a 
-		where diff = a - b
-	let available = Map.differenceWith diffFn maxTimes booked
-	return [ time | (time,_) <- Map.toList available]
+    booked <- bookedTimes
+    let diffFn a b =
+          if diff <= 0 then Nothing
+          else Just a 
+          where diff = a - b
+    let available = Map.differenceWith diffFn maxTimes booked
+    return [ time | (time,_) <- Map.toList available]
+
+registrations :: IO [Registration]
+registrations = do
+    hReg <- openFile "registrations.txt" ReadMode
+    cReg <- hGetContents hReg
+    let sReg = lines cReg
+    return $ map (read :: String -> Registration) sReg
+
+getRegistrationAsJson :: String -> IO String
+getRegistrationAsJson id = do
+    rs <- registrations
+    let reg = filter (\r -> primaryKey r == id) rs
+    case reg of
+        []     -> return ""
+        [r]    -> return $ show $ JsonRegistration r
+        (_:rs) -> return $ show $ JsonRegistration $ last rs
--- haskell/types.hs
+++ haskell/types.hs
@@ -2,6 +2,7 @@ module Santi.Types where
 
 import Data.Text (Text,unpack)
 import qualified Data.Map as Map
+import Data.List (intercalate)
 
 data Registration = Registration
         { name      :: Text
@@ -41,3 +42,34 @@ maxTimes = Map.fromList 	[ ("17:00", 5)
                  		, ("20:30", 5)
                  		]
 
+newtype JsonRegistration = JsonRegistration Registration
+	deriving (Read)
+
+instance Show JsonRegistration where
+	show (JsonRegistration r) = 
+		intercalate "\r\n" $
+		[ "{"
+		, "  \"name\": \"" ++ unpack (name r) ++ "\","
+		, "  \"vorname\": \"" ++ unpack (vorname r) ++ "\","
+		, "  \"strasse\": \"" ++ unpack (strasse r) ++ "\","
+		, "  \"ort\": \"" ++ unpack (ort r) ++ "\","
+		, "  \"telefon\": \"" ++ unpack (telefon r) ++ "\","
+		, "  \"email\": \"" ++ unpack (email r) ++ "\","
+		, "  \"zeit\": \"" ++ unpack (zeit r) ++ "\","
+		, "  \"remarks\": \"" ++ rem ++ "\","
+		, "  \"children\": \"" ++ clds ++ "\","
+		, "}"
+		]
+		where rem = case remarks r of
+			Just t -> unpack t
+			Nothing -> ""
+		      clds = "[ " ++ (intercalate ",\r\n" $ map cld (children r)) ++ " ]"
+		      cld c = intercalate "\r\n  " $
+		              [ "{"
+		              , "  \"childname\": \"" ++ unpack (childname c) ++ "\","
+		              , "  \"childage\": \"" ++ unpack (childage c) ++ "\","
+		              , "  \"childmw\": \"" ++ unpack (childmw c) ++ "\","
+		              , "  \"childpos\": \"" ++ unpack (childpos c) ++ "\","
+		              , "  \"childneg\": \"" ++ unpack (childneg c) ++ "\","
+		              , "}"
+			      ]
