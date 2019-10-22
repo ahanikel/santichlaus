c4d0d88c108b1ff05d3153b6e39e58cf3aa69487
Axel Hanikel <ahanikel@gmail.com>
2019-10-23 01:21:57 +0200

    Zeiten anpassen

--- haskell/src/main/haskell/Santi/Types.hs
+++ haskell/src/main/haskell/Santi/Types.hs
@@ -43,11 +43,13 @@ ckeys = "[\"childname\", \"childage\", \"childmw\", \"childpos\", \"childneg\"]"
 type TimeCount = Map.Map String Int
 
 maxTimes :: TimeCount
-maxTimes = Map.fromList [ ("17:30", 4)
-                        , ("18:00", 4)
-                        , ("18:30", 4)
-                        , ("19:00", 4)
-                        , ("19:30", 4)
+maxTimes = Map.fromList [ ("16:30", 2)
+                        , ("17:00", 2)
+                        , ("17:30", 3)
+                        , ("18:00", 3)
+                        , ("18:30", 3)
+                        , ("19:00", 3)
+                        , ("19:30", 3)
                         ]
 
 data GoogleUser = GoogleUser { guName  :: Text
